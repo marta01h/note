e5f155e5ac44a7389338f1c3d2bc8164236b85af
marta01h <isip_m.a.sargsyan@mpt.ru>
2023-04-10 01:42:07 +0300

    note

---
+++ 1/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System.Timers;
+using System.Windows.Controls;
+using System.Windows;
+using System;
+
+public partial class MainWindow : Window
+{
+
+    public MainWindow()
+    {
+        InitializeComponent();
+        Timer t = new Timer(1000);
+        t.Elapsed += T_Elapsed;
+        t.Start();
+    }
+
+    private void T_Elapsed(object sender, ElapsedEventArgs e)
+    {
+        DateTime ci = DateTime.Now;
+        this.Dispatcher.Invoke(new Action(() => this.Label1.Content = ci.ToLongTimeString()));
+    }
+
+    private void Calendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+    {
+        // ... Get reference.
+        var calendar = sender as Calendar;
+
+        // ... See if a date is selected.
+        if (calendar.SelectedDate.HasValue)
+        {
+            // ... Display SelectedDate in Title.
+            DateTime date = calendar.SelectedDate.Value;
+            this.Title = date.ToShortDateString();
+
+        }
+
+    }
---
+++ 1/Class2.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows;
+using System.Xml.Serialization;
+
+namespace _1namespace WpfApplication3
+{
+    [Serializable]
+public class Task
+{
+    public string ToDo { get; set; }
+    public DateTime Time { get; set; }
+    public Task() { }
+
+}
+
+[Serializable]
+public class Day
+{
+    public List<Task> ListTasks { set; get; }
+    public DateTime Date { get; set; }
+    public Day() { }
+
+}
+
+public partial class MainWindow : Window
+{
+
+    List<Day> lst = new List<Day>();
+
+    public MainWindow()
+    {
+        InitializeComponent();
+
+    }
+
+
+    private void Calendar_SelectedDatesChanged(object sender, SelectionChangedEventArgs e)
+    {
+
+        var calendar = sender as Calendar;
+
+        if (calendar.SelectedDate.HasValue)
+        {
+
+            var arDays = this.lst.Where(x => calendar.SelectedDate.Value.Date == x.Date).Cast<Day>();
+            foreach (var oneDay in arDays)
+            {
+                int iCount = 0;
+                foreach (TextBox tb in this.StEdits.Children.Cast<TextBox>())
+                    tb.Text = oneDay.ListTasks[iCount++].ToDo;
+
+            }
+
+        }
+
+    }
+
+    void Serialize()
+    {
+        try
+        {
+            using (FileStream fs = new FileStream("1.xml", FileMode.OpenOrCreate, FileAccess.Write))
+            {
+                XmlSerializer xml = new XmlSerializer(typeof(List<Day>));
+                xml.Serialize(fs, lst);
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+        }
+    }
+    void Deserialize()
+    {
+        try
+        {
+            using (FileStream fs = new FileStream("1.xml", FileMode.Open, FileAccess.Read))
+            {
+                XmlSerializer xml = new XmlSerializer(typeof(List<Day>));
+                this.lst = (List<Day>)xml.Deserialize(fs);
+            }
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show(ex.Message);
+        }
+    }
+
+}
+}
+
+{
+    internal class Class2
+    {
+    }
+}
